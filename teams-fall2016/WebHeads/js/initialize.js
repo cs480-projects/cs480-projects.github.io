c52d7769d42de0ac3564e8a062d2ee1fa1b2d325
Christine <christine.tn.nguyen@gmail.com>
2016-10-10 21:08:57 -0700

    edit WebHeads site design

---
+++ teams-fall2016/WebHeads/js/initialize.js
@@ -0,0 +1,9 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+    "use strict";
+    $('#wrapper').fullpage({
+        anchors:['sect1', 'sect2']
+    });
+});
