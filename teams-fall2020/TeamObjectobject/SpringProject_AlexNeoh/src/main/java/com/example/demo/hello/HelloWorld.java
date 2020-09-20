eef67225b81c2346561562d7ccc0373161127bde
aneoh <39381481+aneoh@users.noreply.github.com>
2020-09-19 19:57:04 -0700

    SpringProject for Alex Neoh

---
+++ teams-fall2020/TeamObjectobject/SpringProject_AlexNeoh/src/main/java/com/example/demo/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.demo.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	@RequestMapping(value="/", method= RequestMethod.GET )
+	public String hello() {
+		return "Hello World";
+	}
+}
