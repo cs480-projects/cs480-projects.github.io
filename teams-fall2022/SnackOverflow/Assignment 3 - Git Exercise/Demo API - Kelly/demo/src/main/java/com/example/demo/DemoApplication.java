c71bcdf882dd38990b10ab2c4fc23f06396e37dc
Kelly <kellytsai1997@gmail.com>
2022-09-30 22:17:38 -0700

    added kelly assignment 3

---
+++ teams-fall2022/SnackOverflow/Assignment 3 - Git Exercise/Demo API - Kelly/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	private static final String POSTS_API_URL = "https://jsonplaceholder.typicode.com/posts";
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		//SpringApplication.run(DemoApplication.class, args);
+
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.GET()
+				.header("accept", "appplication/json")
+				.uri(URI.create(POSTS_API_URL))
+				.build();
+
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		System.out.println(response.body());
+	}
+}
