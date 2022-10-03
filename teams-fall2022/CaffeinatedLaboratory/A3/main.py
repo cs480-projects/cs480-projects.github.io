863dfc77bcf6da098612fb6e310550d8940b947b
Moina Vernon <mvernon@cpp.edu>
2022-10-02 23:19:44 -0700

    Add main.py

---
+++ teams-fall2022/CaffeinatedLaboratory/A3/main.py
@@ -0,0 +1,23 @@
+from flask import *
+import json, time
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home_page():
+	data_set = {'Page': 'Home',
+             f'Message': 'Hello Anonymous!', 'Timestamp': time.time()}
+	return json.dumps(data_set)
+
+@app.route('/name/', methods=['GET'])
+def name_page():
+	name_query = str(request.args.get('name'))  # /name/?name=SOMENAME
+	data_set = {'Page': 'Welcome',
+             f'Message': f'Hello {name_query}!', 'Timestamp': time.time()}
+	return json.dumps(data_set)
+
+if __name__ == '__main__':
+	app.run(port=8080)
+
+#ngrok http 127.0.0.1:8080 --host-header="127.0.0.1:8080"
