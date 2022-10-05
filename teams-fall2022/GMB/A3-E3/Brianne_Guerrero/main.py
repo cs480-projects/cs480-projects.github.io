381a0bd5575346b668a005297ab29b3cc825cb95
bnguerrero <89376675+bnguerrero@users.noreply.github.com>
2022-10-04 20:37:52 -0700

    update main.py

--- teams-fall2022/GMB/A3-E3/Brianne_Guerrero/main.py
+++ teams-fall2022/GMB/A3-E3/Brianne_Guerrero/main.py
@@ -1,1 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def intro():
+    return "This is Brianne's homepage."
+
+@app.route("/hello/<world>")
+def hello(world):
+    return "Hello" + world + "welcome to my website"
+
+app.run(host='0.0.0.0')
 
