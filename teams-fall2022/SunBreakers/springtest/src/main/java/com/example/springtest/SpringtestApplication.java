9a37a3177e45361bb0f11efafd7019aead9619f7
tytetech <65252893+tytetech@users.noreply.github.com>
2022-10-04 21:36:07 -0700

    HTTP API method addition (A3-3)

--- teams-fall2022/SunBreakers/springtest/src/main/java/com/example/springtest/SpringtestApplication.java
+++ teams-fall2022/SunBreakers/springtest/src/main/java/com/example/springtest/SpringtestApplication.java
@@ -3,6 +3,8 @@ package com.example.springtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @RestController
@@ -10,5 +12,10 @@ public class SpringtestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtestApplication.class, args);
+	}
+	
+	@GetMapping("/howdy")
+	public String howdy(@RequestParam(value = "name", defaultValue = "Partner") String name) {
+	return String.format("Howdy %s!", name);
 	}
 }
