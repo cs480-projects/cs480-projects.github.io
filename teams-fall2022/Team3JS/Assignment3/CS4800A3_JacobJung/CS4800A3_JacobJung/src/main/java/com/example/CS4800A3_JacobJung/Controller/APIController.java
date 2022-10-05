a5a146acd33fa0f0234216e25cd789f796b769e2
Jhelsing1 <frostproxiide@gmail.com>
2022-10-04 19:44:22 -0700

    Added new site
    It returns a random fraction on the page

--- teams-fall2022/Team3JS/Assignment3/CS4800A3_JacobJung/CS4800A3_JacobJung/src/main/java/com/example/CS4800A3_JacobJung/Controller/APIController.java
+++ teams-fall2022/Team3JS/Assignment3/CS4800A3_JacobJung/CS4800A3_JacobJung/src/main/java/com/example/CS4800A3_JacobJung/Controller/APIController.java
@@ -1,8 +1,11 @@
 package com.example.CS4800A3_JacobJung.Controller;
 
+import java.util.Random;
+import org.apache.commons.math3.fraction.Fraction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.*;
 
 @Controller
 public class APIController {
@@ -12,4 +15,21 @@ public class APIController {
     public String api(){
         return "Hello World from Jacob!";
     }
+    
+    @ResponseBody
+    @GetMapping("/api/hello2")
+    public String api2()
+    {
+        Random r = new Random();
+        double randomValue = 1 * r.nextDouble();
+        Fraction a = RandomFraction(randomValue);
+        String retval = a.toString();
+        return "<div class=\"center\" align=\"center\">" + "<h1>Here's a random fraction: " +  randomValue + " = " + retval + "</h1>";
+    }
+    public static Fraction RandomFraction(double randomValue)
+    {
+        Fraction fraction = new Fraction(randomValue);
+        return fraction;
+    }
+
 }
