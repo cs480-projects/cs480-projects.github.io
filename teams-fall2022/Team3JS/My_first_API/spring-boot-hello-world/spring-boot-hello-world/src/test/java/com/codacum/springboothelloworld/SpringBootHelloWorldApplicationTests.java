b3803ed00b077110dc94f70444c7dfcb2d671bf2
josue0128 <josuec@cpp.edu>
2022-10-04 19:55:49 -0800

    Added my first API application

---
+++ teams-fall2022/Team3JS/My_first_API/spring-boot-hello-world/spring-boot-hello-world/src/test/java/com/codacum/springboothelloworld/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codacum.springboothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
