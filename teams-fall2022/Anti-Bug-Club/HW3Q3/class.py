5e5bbbdc35f5c3ac782947e535d3b0cb38b8d7f2
Phuong Thai <82252139+eannel@users.noreply.github.com>
2022-10-04 20:38:26 -0700

    Update class.py

--- teams-fall2022/Anti-Bug-Club/HW3Q3/class.py
+++ teams-fall2022/Anti-Bug-Club/HW3Q3/class.py
@@ -1,10 +1,10 @@
 from flask import Flask
-import random
+
 
 app =   Flask(__name__)
   
 @app.route("/")
-def webpage():
+def message():
     return "Hello World!, I am Phuong Thai."
     
 if __name__=='__main__':
