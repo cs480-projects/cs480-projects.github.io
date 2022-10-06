83df28f15527b8bb5fa0636053b26fdeeed30147
julianrowe <julianrowe123@gmail.com>
2022-10-06 13:43:40 -0700

    Added a method that uses JSoup

--- teams-fall2022/SunBreakers/springtest/src/main/java/com/example/springtest/SpringtestApplication.java
+++ teams-fall2022/SunBreakers/springtest/src/main/java/com/example/springtest/SpringtestApplication.java
@@ -3,8 +3,6 @@ package com.example.springtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @RestController
@@ -12,23 +10,5 @@ public class SpringtestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtestApplication.class, args);
-	}
-	//Duc Thanh Nguyen
-	 @GetMapping("/hi")
-	 public String hi(@RequestParam(value = "name", defaultValue = "a good day!!!") String name) {
-	 	return String.format("Haveeeeee %s!", name);
-	 }
-	
-	//Noe Rivera
-	@GetMapping("/aboutus")
-	public String aboutus(@RequestParam(value = "name", defaultValue = "get to know our team!") String name) 
-	{
-		return String.format("Welcome future SunBreaker, %s!", name);
-	}
-	
-	//Tyler Kloss
-	@GetMapping("/howdy")
-	public String howdy(@RequestParam(value = "name", defaultValue = "Partner") String name) {
-		return String.format("Howdy %s!", name);
 	}
 }
