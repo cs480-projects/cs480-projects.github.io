0b8d2aed6e44ba496aa85a2741332b2924c0ff8e
Lily Hou <79422033+lilyh123@users.noreply.github.com>
2022-10-06 22:14:23 -0700

    added a method that will render html template

--- teams-fall2022/GMB/A3-E3/Lily_Hou_Wu/main.py
+++ teams-fall2022/GMB/A3-E3/Lily_Hou_Wu/main.py
@@ -1,21 +1,22 @@
-from flask import Flask
-app = Flask('app')
+from flask import Flask, render_template
+from datetime import datetime
+import re
+app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-  return 'Welcome'
+    return 'Welcome to a new page!'
 
 @app.route('/hi')
 def hello_world():
-	return 'Hello World!'
-
-@app.route('/about')
-def about():
-  return 'This is a new page'
+    return 'Hello World!'
 
 @app.route('/guest/<guest>')
 def hello_guest(guest):
-   return 'Hello ' + guest
+    return 'Hello ' + guest + 'welcome to this new page!'
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 app.run(host='0.0.0.0')
