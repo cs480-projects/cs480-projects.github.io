824d64c20b8486835dc10630900e747740cc79af
Matthew Ngoi <mfngoi3@gmail.com>
2022-10-06 17:35:46 -0700

    Added api method to find list of collections in database

--- teams-fall2022/GMB/A3-E3/Matthew_Ngoi/server.py
+++ teams-fall2022/GMB/A3-E3/Matthew_Ngoi/server.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://mfngoi:wGfna56m1cxs2Sag@cluster0.ltgs7ps.mongodb.net/?retryWrites=true&w=majority")
+db = client.queues
 
 @app.route("/")
 def home():
@@ -13,5 +17,9 @@ def hello():
 @app.route("/hello/<name>")
 def greeting(name):
     return "Hello " + name
+
+@app.route("/mongo")
+def mongo():
+    return db.list_collection_names()
 
 app.run(host='0.0.0.0')
