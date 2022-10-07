8b1068b4ce3eec975adbf4eb39203acb9e238ac6
Jezreel Salinas <jsalinas@cpp.edu>
2022-10-06 19:39:09 -0700

    Fixed typo for corsheader

--- teams-fall2022/blueEggs/A3Q3/JezA4/Assignment4/BackEnd/myAssignemnt/myAssignemnt/settings.py
+++ teams-fall2022/blueEggs/A3Q3/JezA4/Assignment4/BackEnd/myAssignemnt/myAssignemnt/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheader',
+    'corsheaders',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
