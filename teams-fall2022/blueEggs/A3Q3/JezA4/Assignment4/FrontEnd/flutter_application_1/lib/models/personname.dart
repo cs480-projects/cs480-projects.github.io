c22dc68dbbc2737f3d78d29ebb7983b2a0b81604
Jezreel Salinas <jsalinas@cpp.edu>
2022-10-06 20:39:38 -0700

    implemented an api and models for front end

---
+++ teams-fall2022/blueEggs/A3Q3/JezA4/Assignment4/FrontEnd/flutter_application_1/lib/models/personname.dart
@@ -0,0 +1,14 @@
+
+
+
+class PersonName{
+  final String firstName, lastName; //Stores person's name
+  PersonName({required this.firstName, required this.lastName}); /// OVerloading constructor
+
+
+  /// assigned the name from the site by translating json to this
+  factory PersonName.fromJson(Map<String, dynamic> json){
+    firstName : json['firstName'];
+    lastName : json['lastName'];
+  }
+}
