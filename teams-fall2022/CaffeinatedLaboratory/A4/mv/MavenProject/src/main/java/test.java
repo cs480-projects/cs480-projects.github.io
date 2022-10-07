1cc1981ed1c37e6ae168828bf34a00a98f5f2a6a
Moina Vernon <mvernon@cpp.edu>
2022-10-06 20:12:55 -0700

    use jackson dep

--- teams-fall2022/CaffeinatedLaboratory/A4/mv/MavenProject/src/main/java/test.java
+++ teams-fall2022/CaffeinatedLaboratory/A4/mv/MavenProject/src/main/java/test.java
@@ -4,7 +4,14 @@ public class test {
 
 	public static void main(String args[]) 
 	{
-		System.out.println("Hello World");
+		String simpleJson = "{\n" + "	\"name\":\"Person1\"," + "	\"available\":\"13:00\" " + "}";
 
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode jsonNode = mapper.readTree(simpleJson.getBytes());
+
+		String name = jsonNode.get("name").textValue();
+
+		System.out.println("Name: " + name);
+		System.out.println("Available: " + jsonNode.get("available").textValue());
 	}
 }
