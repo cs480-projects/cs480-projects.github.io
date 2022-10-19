9bd8bb51cacdbbfb434aee5c65d42206ba6a223b
Lily Hou <79422033+lilyh123@users.noreply.github.com>
2022-10-19 00:30:16 -0700

    added unit test for app route

---
+++ teams-fall2022/GMB/A3-E3/Lily_Hou_Wu/A6/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from datetime import datetime
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return 'Welcome to a new page!'
+
+
+@app.route('/hi')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return 'Hello ' + guest + 'welcome to this new page!'
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+app.run(host='0.0.0.0')
