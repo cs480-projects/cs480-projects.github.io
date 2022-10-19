bd685646002525c715669c82fd4ca37590344ccf
Trina Hoang Ngo <trinahngo311@gmail.com>
2022-10-18 22:01:15 -0700

    junit test

---
+++ teams-fall2022/CaffeinatedLaboratory/A6/tn-JUnit/test/src/main/java/com/example/Calculator.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Calculator {
+    public int evaluate(String expression) {
+        int sum = 0;
+        for (String summand : expression.split("\\+"))
+            sum += Integer.valueOf(summand);
+            return sum;
+    } 
+}
