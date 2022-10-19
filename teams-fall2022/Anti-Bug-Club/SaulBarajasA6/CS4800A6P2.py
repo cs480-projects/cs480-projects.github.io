dd1c1cf23989a60610878ee407931ec34ec895b0
SaulBarajas <98624770+SaulBarajas@users.noreply.github.com>
2022-10-18 23:00:24 -0700

    Added new methods

--- teams-fall2022/Anti-Bug-Club/SaulBarajasA6/CS4800A6P2.py
+++ teams-fall2022/Anti-Bug-Club/SaulBarajasA6/CS4800A6P2.py
@@ -19,7 +19,6 @@ class TestGetAreaRectangleWithSetUp(unittest.TestCase):
  
   @classmethod
   def setUpClass(self):
-    #this method is only run once for the entire class rather than being run for each test which is done for setUp()
     self.rectangle = Rectangle(0, 0)
  
   def test_normal_case(self):
@@ -28,11 +27,9 @@ class TestGetAreaRectangleWithSetUp(unittest.TestCase):
     self.assertEqual(self.rectangle.get_area(), 6, "incorrect area")
  
   def test_geq(self):
-    """tests if value is greater than or equal to a particular target"""
     self.assertGreaterEqual(self.rectangle.get_area(), -1)
  
   def test_assert_raises(self): 
-    """using assertRaises to detect if an expected error is raised when running a particular block of code"""
     with self.assertRaises(ZeroDivisionError):
       a = 1 / 0
  
