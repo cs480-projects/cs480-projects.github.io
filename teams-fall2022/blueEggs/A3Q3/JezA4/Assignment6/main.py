8ff3196de8e2ddc379c5491f1422b753d608350c
Jezreel Salinas <jsalinas@cpp.edu>
2022-10-18 18:59:11 -0700

    Made unit testing more specific

--- teams-fall2022/blueEggs/A3Q3/JezA4/Assignment6/main.py
+++ teams-fall2022/blueEggs/A3Q3/JezA4/Assignment6/main.py
@@ -1,7 +1,17 @@
 from unitTesting import UnitTesting as ut
 
+
+
 ut.test_equal('ABC', 'ABC')
 
 ut.test_num_equal((3*2)+4, 10)
+
+a = 2
+b = 4
+
+ut.testAdd(a,b, 6)
+ut.testSub(a,b, -2)
+ut.testMul(a,b, 8)
+ut.testDiv(a,b, 0.5)
 
 print('Success')
--- teams-fall2022/blueEggs/A3Q3/JezA4/Assignment6/unitTesting.py
+++ teams-fall2022/blueEggs/A3Q3/JezA4/Assignment6/unitTesting.py
@@ -1,9 +1,23 @@
 import numpy as np
+from myTest import myCalculator as mt
 
 class UnitTesting:
     def test_equal(str1, str2):
         np.testing.assert_string_equal(str1, str2)
     def test_num_equal(num1, num2):
         np.testing.assert_equal(num1, num2)
+
+    def testAdd(num1, num2, expected):
+        np.testing.assert_equal(mt.addTwo(num1, num2), num1 + num2)
+
+    def testSub(num1, num2, expected):
+        np.testing.assert_equal(mt.subTwo(num1, num2), num1 - num2)
+
+    def testMul(num1, num2, expected):
+        np.testing.assert_equal(mt.multTwo(num1, num2), num1 * num2)
+    
+    def testDiv(num1, num2, expected):
+        np.testing.assert_equal(mt.divTwo(num1, num2), num1 / num2)
+    
     
 
