a2107074df738906c313de70a06af2c5fd7fd05c
Ratske99 <sethlaske9999@gmail.com>
2023-02-23 18:25:37 -0800

    A4 Seth
    Added Apache Commons

--- teams-spring2023/Bouldering-Baddies/backend/src/main/java/com/boulderingbaddies/tsabackend/TsaBackendController.java
+++ teams-spring2023/Bouldering-Baddies/backend/src/main/java/com/boulderingbaddies/tsabackend/TsaBackendController.java
@@ -1,7 +1,17 @@
 package com.boulderingbaddies.tsabackend;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import java.util.Arrays;
 
 @RestController
 public class TsaBackendController {
@@ -25,4 +35,21 @@ public class TsaBackendController {
     public String joshua() {
         return "It would be hilarious if I messed up Seth's commit";
     }
+
+    @RequestMapping("/apache")
+    public String apache() {
+
+        List<Double> data = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0);
+
+        Mean mean = new Mean();
+        double dataMean = mean.evaluate(data.stream().mapToDouble(Double::doubleValue).toArray());
+        System.out.println("Mean: " + dataMean);
+
+        StandardDeviation std = new StandardDeviation();
+        double dataStd = std.evaluate(data.stream().mapToDouble(Double::doubleValue).toArray());
+        System.out.println("Standard deviation: " + dataStd);
+        String result = "Standard deviation: " + dataStd;
+        return result;
+    }
+
 }
