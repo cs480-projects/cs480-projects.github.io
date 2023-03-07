93092d00f0cc7ec392567970e2e5033af7d18d2a
Faisal-Syed <fas2001@g4vxvhn7qj.lan>
2023-03-07 11:00:12 -0800

    Added JUnit tests for Aiport API

--- teams-spring2023/Bouldering-Baddies/backend/src/test/java/com/boulderingbaddies/tsabackend/TsaBackendApplicationTests.java
+++ teams-spring2023/Bouldering-Baddies/backend/src/test/java/com/boulderingbaddies/tsabackend/TsaBackendApplicationTests.java
@@ -3,11 +3,44 @@ package com.boulderingbaddies.tsabackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest;
+import java.net.http.HttpClient;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TsaBackendApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testAirportSNAExistsInAPI()
+            throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://airport-info.p.rapidapi.com/airport?iata=SNA"))
+                .header("X-RapidAPI-Key", "b0d3c98a37mshaf71740f91fe6ffp1c2bdejsnf8e4edef876b")
+                .header("X-RapidAPI-Host", "airport-info.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        assertEquals(response.statusCode(), 200);
+    }
+
+    @Test
+    public void testAirportLAXExistsInAPI()
+            throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://airport-info.p.rapidapi.com/airport?iata=LAX"))
+                .header("X-RapidAPI-Key", "b0d3c98a37mshaf71740f91fe6ffp1c2bdejsnf8e4edef876b")
+                .header("X-RapidAPI-Host", "airport-info.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        assertEquals(response.statusCode(), 200);
     }
 
 }
