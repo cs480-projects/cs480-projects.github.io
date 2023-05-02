e5440d4a1b1d72f8467895cc91c1211632babd2d
fidelisprasetyo <fidelisprasetyo@gmail.com>
2023-05-02 12:05:27 -0700

    updated raccoons team page

---
+++ teams-spring2023/Raccoons/js/scripts.js
@@ -0,0 +1,10 @@
+/*!
+* Start Bootstrap - Coming Soon v6.0.7 (https://startbootstrap.com/theme/coming-soon)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+$('.carousel').carousel({
+    interval: 2000
+})
