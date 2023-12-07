23f21064c07647a75d475fd10148f8326511d7c9
Tnguyend1 <ukeynguyen@gmail.com>
2023-12-06 17:24:44 -0800

    update

--- teams-fall2023/HTD-4800/sp.js
+++ teams-fall2023/HTD-4800/sp.js
@@ -1,5 +1,5 @@
 var imageIndex = 1;
-var images = ["loginscreen.png", "projectimage.png"];
+var images = ["loginscreen.png", "em.png", "lostpet.png"];
 var imageElement = document.getElementById("transitionImage");
 
 function transitionImage() {
