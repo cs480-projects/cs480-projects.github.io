e4094cecc7d51e77593a911dbe4beaf674c344e2
uuriah <132951774+uuriah@users.noreply.github.com>
2025-09-20 23:14:01 -0700

    Create a home url that displays a welcome message.

--- teams-fall2025/ByteWorxx/backend/home_page/views.py
+++ teams-fall2025/ByteWorxx/backend/home_page/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to our home page!")
+    return HttpResponse("Welcome to our home page!<br/>We hope you can find some good recipes.")
